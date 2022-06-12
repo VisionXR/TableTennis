1639f80d658f3e96a4be58332d81ec93e72ff9b6
VisionXR <86660229+VisionXR@users.noreply.github.com>
2022-06-12 16:21:03 +0530

    16:20

--- TableTennis/Assets/Scripts/NetworkManager.cs
+++ TableTennis/Assets/Scripts/NetworkManager.cs
@@ -3,23 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class NetworkManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     // Start is called before the first frame update
     void Start()
     {
-        ConnectToServer();
+        roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsOpen = true;
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    private void ConnectToServer()
+
+    public void OnJoinButtonClicked()
     {
-        PhotonNetwork.AddCallbackTarget(this);
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.JoinRandomOrCreateRoom(roomOptions: roomOptions);
     }
+    public void OnEvent(EventData photonEvent)
+    {
+
+
+    }
+
+    #region PhotonCallBacks
+    private void OnConnectedToServer()
+    {
+        Debug.Log(" connected to server");
+    }
+    #endregion
 }
